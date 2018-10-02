31dd9248b18950453a207d70648b5762201aa533
Joe Stringer <joe@wand.net.nz>
2018-10-02 13:35:31 -0700

    UPSTREAM: bpf: Reuse canonical string formatter for ctx errs
    The array "reg_type_str" provides canonical formatting of register
    types, however a couple of places would previously check whether a
    register represented the context and write the name "context" directly.
    An upcoming commit will add another pointer type to these statements, so
    to provide more accurate error messages in the verifier, update these
    error messages to use "reg_type_str" instead.
    
    Change-Id: I70f18532bfb93c107268b4f91d3ae78bd847894b
    Signed-off-by: Joe Stringer <joe@wand.net.nz>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -2048,8 +2048,7 @@ static int check_xadd(struct bpf_verifier_env *env, int insn_idx, struct bpf_ins
 	    is_pkt_reg(env, insn->dst_reg) ||
 	    is_sk_reg(env, insn->dst_reg)) {
 		verbose(env, "BPF_XADD stores into R%d %s is not allowed\n",
-			insn->dst_reg, is_ctx_reg(env, insn->dst_reg) ?
-			"context" : "packet");
+			insn->dst_reg, reg_type_str[insn->dst_reg]);
 		return -EACCES;
 	}
 
@@ -5898,8 +5897,8 @@ static int do_check(struct bpf_verifier_env *env)
 				return err;
 
 			if (is_ctx_reg(env, insn->dst_reg)) {
-				verbose(env, "BPF_ST stores into R%d context is not allowed\n",
-					insn->dst_reg);
+				verbose(env, "BPF_ST stores into R%d %s is not allowed\n",
+					insn->dst_reg, reg_type_str[insn->dst_reg]);
 				return -EACCES;
 			}
 
